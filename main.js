fc03aed5564911bacd26b7bcfe2c9d42d4e31ac7
Adrien El Zein <adrien.elzein@zelros.com>
2019-03-29 00:33:07 +0100

    feat(text): add main sections of the site

---
+++ main.js
@@ -0,0 +1,298 @@
+var OBJECT_TO_CHAPTERS = {
+    'RAINBOW_BLOB': 'ethos',
+    'Torus.2': 'infos',
+    'cube': 'contributions'
+}
+
+
+if ( WEBGL.isWebGLAvailable() === false ) {
+    document.body.appendChild( WEBGL.getWebGLErrorMessage() );
+}
+
+var scene = new THREE.Scene();
+
+var fov = 30;
+var aspect = window.innerWidth / window.innerHeight;
+var near = 0.1;
+var far = 100;
+
+
+var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+var renderer = new THREE.WebGLRenderer({alpha:true });
+
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+document.body.appendChild( renderer.domElement );
+
+
+
+///// OBJECTS
+
+var geometryCube = new THREE.BoxGeometry(1, 1, 1);
+var materialCube = new THREE.MeshLambertMaterial( { color: 0x2233ee } );
+
+var geometrySphere = new THREE.SphereGeometry(1, 32, 32);
+var materialGlobe = new THREE.MeshPhongMaterial({
+  wireframe : true
+});
+var cube = new THREE.Mesh( geometryCube, materialCube );
+var sphere = new THREE.Mesh( geometrySphere, materialGlobe );
+var flubber;
+var torus;
+
+
+
+var scale = 0.5;
+cube.scale.set(scale, scale, scale);
+cube.position.x = 0.7;
+cube.name = 'cube';
+
+
+sphere.position.x = 0;
+sphere.name = 'sphere';
+
+scene.add( cube );
+scene.add( sphere );
+
+//scene.add( cube1 );
+//scene.add( cube2 );
+camera.position.z = 6;
+
+//cube.position.x = Math.cos(time)/1;
+
+//mesh.rotation.y = THREE.Math.degToRad(45);
+
+
+/////// LIGHTS
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+var light = new THREE.DirectionalLight(0xffffff, 0.9);
+light.castShadow = true;
+scene.add(light);
+scene.add(ambientLight);
+
+
+
+///// BACKGROUND
+var geometry = new THREE.BoxBufferGeometry(50, 50, 50);
+// invert the geometry on the x-axis so that all of the faces point inward
+geometry.scale( - 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( {
+    map: new THREE.TextureLoader().load( 'textures/background_2.jpg' )
+} );
+var mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+
+
+
+/////// CONTROLS
+
+controls = new THREE.OrbitControls( camera );
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.enableRotate = true;
+
+/*
+controls.minDistance = 1
+controls.maxDistance = 100
+
+controls.minPolarAngle
+controls.maxPolarAngle
+*/
+
+
+/////// INTERACTIONS
+
+var mouse = new THREE.Vector2(), INTERSECTED;
+document.addEventListener( 'mousemove', function () {
+    event.preventDefault();
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}, false );
+
+var raycaster = new THREE.Raycaster();
+var intersects;
+var contentHidden = true;
+
+
+/////// GROUP
+/*
+var group = new THREE.Group();
+scene.add( group );
+
+cube.name = 'cube';
+group.add( cube );
+group.add( sphere );
+
+
+mesh2.visible = false;
+group.remove( mesh2 );
+
+group.children // mesh1
+group.parent // scene
+*/
+
+// ASSETS LOADING
+
+
+function loadModel() {
+
+}
+
+var manager = new THREE.LoadingManager( loadModel );
+
+manager.onProgress = function ( item, loaded, total ) {
+    console.log( item, loaded, total );
+};
+
+// texture
+var textureLoader = new THREE.TextureLoader( manager );
+var texture = textureLoader.load( 'textures/gradient.jpeg' );
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 1, 1 );
+
+// model
+function onProgress( xhr ) {
+    if ( xhr.lengthComputable ) {
+        var percentComplete = xhr.loaded / xhr.total * 100;
+        console.log( 'model ' + Math.round( percentComplete, 2 ) + '% downloaded' );
+    }
+}
+
+function onError(err) {
+    console.error(err)
+}
+
+var loader = new THREE.OBJLoader( manager ).setPath('assets/');
+
+loader.load( 'rainbow_flubber.obj', function ( obj ) {
+    flubber = obj;
+    flubber.traverse( function ( child ) {
+        if ( child.isMesh ) child.material.map = texture;
+    } );
+    flubber.position.y = - 0.8;
+    flubber.scale.set(0.01, 0.01, 0.01);
+    scene.add( flubber );
+}, onProgress, onError );
+
+loader.load( 'torus_01.obj', function ( obj ) {
+    torus = obj;
+    torus.scale.set(0.002, 0.002, 0.002);
+    torus.position.x = - 0.8;
+    scene.add( torus );
+}, onProgress, onError );
+
+
+/*
+var loader_gltf = new THREE.GLTFLoader().setPath('assets/');
+
+loader_gltf.load('Duck.glb',
+    function (gltf) {
+        gltf.scene.scale.set(0.5, 0.5, 0.5);
+        gltf.scene.position.x = -0.8;
+        scene.add(gltf.scene);
+    },
+    function (xhr) {
+        console.log('loading', xhr.loaded * 100 / xhr.total);
+    },
+    function (error) {
+        console.error(error);
+    }
+);
+
+
+var loader = new THREE.OBJLoader().setPath('assets/');
+
+
+// load a resource
+loader.load(
+    // resource URL
+    'rainbow_flubber.obj',
+    // called when resource is loaded
+    function ( object ) {
+        //flubber.scene.scale.set(0.1, 0.1, 0.1);
+        //object.scene.position.x = -0.8;
+        object.scale.set(0.01, 0.01, 0.01);
+        object.position.y = -0.8;
+        flubber = object;
+    },
+    // called when loading is in progresses
+    function ( xhr ) {
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+    },
+    // called when loading has errors
+    function ( error ) {
+
+        console.log( 'An error happened' );
+
+    }
+);
+*/
+
+window.onresize = function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+};
+
+document.addEventListener('click', function (event) {
+    if (INTERSECTED) {
+        //alert('you clicked on the cube !')
+        console.log(INTERSECTED.name);
+        showContent(OBJECT_TO_CHAPTERS[INTERSECTED.name]);
+    }
+});
+
+function render() {
+    requestAnimationFrame(render);
+
+    raycaster.setFromCamera( mouse, camera );
+    intersects = raycaster.intersectObjects([flubber, cube, torus], true);
+
+    if( intersects.length > 0 && contentHidden) {
+        if ( INTERSECTED !== intersects[ 0 ].object ) {
+            //alert('you intersected ' + intersects)
+            //if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+            INTERSECTED = intersects[ 0 ].object;
+            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+            INTERSECTED.material.emissive.setHex( 0x006600 );
+        }
+    } else {
+        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+        INTERSECTED = null;
+    }
+
+    controls.update();
+    renderer.render( scene, camera );
+}
+render();
+
+window.scene = scene;
+
+function showContent(chapter){
+    console.log(chapter);
+    var container = document.getElementById('content-container');
+    var chapter_div = document.getElementById(chapter);
+    var active_div = container.getElementsByClassName('active');
+    for (i = 0; i < active_div.length; i++) {
+        active_div[i].classList.remove('active');
+    }
+    chapter_div.classList.add("active");
+    container.classList.add('active');
+    contentHidden = false;
+}
+
+function closeContent() {
+    var container = document.getElementById('content-container');
+    var active_div = container.getElementsByClassName('active');
+    for (i = 0; i < active_div.length; i++) {
+        active_div[i].classList.remove('active');
+    }
+    container.classList.remove('active');
+    contentHidden = true;
+}
+
