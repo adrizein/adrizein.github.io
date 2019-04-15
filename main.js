27586f241cb5c846cbfc3582183a190715ab0599
Julien Vinckel <julien.vinckel@gmail.com>
2019-04-15 18:13:55 +0200

    slower rotation

--- main.js
+++ main.js
@@ -250,7 +250,7 @@ $(function() {
     load_objects();
 
     function rotate_object(object) {
-        var SPEED = 0.01;
+        var SPEED = 0.005;
         object.rotation.x -= SPEED * 2;
         object.rotation.y -= SPEED;
         object.rotation.z -= SPEED * 3;
