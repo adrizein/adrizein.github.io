d99f2ec28671c3a523b51b22b618b17650b6e5d9
Julien Vinckel <julien.vinckel@gmail.com>
2020-02-11 22:14:07 +0100

    fading working approximately

--- js/main.js
+++ js/main.js
@@ -37,9 +37,60 @@ function updateState(currentSectionWithId) {
 function updateBackground(currentSectionWithId) {
     /* Where we manage the backgrounds */
 
-    var backgroundsOnTop = document.getElementsByClassName("top");
+    var backgroundCurrentlyOnTop = document.getElementsByClassName("top")[0];
     var backgroundToPutOnTop = document.getElementById("bg-"+currentSectionWithId.id);
+    var currentSection = document.getElementById(currentSectionWithId.id);
+    var spacer = document.getElementsByClassName("spacer")[0]; // To improve 
 
+    /* Here is the sequence 
+    * 1 - Home, distance 0 scale of one
+
+    * A section has a padding top of one hundred
+    * When we start the beginning of a section the background starts appearing at a scale max
+    * and the previous one starts disappearing
+    * When we reach the spacer the end of the section 
+    * The zoom in zoom out occurs in the spacer
+
+    * 2 - When reaching the end of the spacer (the beginning of the new section) we reach a zoom of zoom max
+    * 3 - we stop the zoom and start fading out the background and fading in the new one at zoom max
+    * 4 - when reaching the end of the section the old background is completely faded out and the new one is faded in
+    * 5 - we start zooming out until we reach the scale of one at spacerheight/2 and zoom back in to reach zoom max at the end of the <spacer className=""></spacer>
+    * 6 - We go back to 3
+    */
+
+   var scale = 1;
+   var opacity = 1;
+   var scaleMax = 6;
+   var a, b;
+    if (currentSectionWithId.id == "home") {
+        if (backgroundCurrentlyOnTop != undefined) {backgroundCurrentlyOnTop.classList.remove("top")} ;
+        backgroundToPutOnTop.classList.add("top");
+        /* scale of 1 at distance 0
+        * scaleMax at home section height + spacer height
+        */
+        b = 1
+        a = (scaleMax-1)/(currentSection.clientHeight + spacer.clientHeight)
+        scale = a * currentSectionWithId.distance + b
+        backgroundToPutOnTop.style.transform = 'scale(' + scale + ')';
+    } else {
+        if (currentSectionWithId.distance < currentSection.clientHeight) {
+            /* We don't scale in the section and the scale stays at scaleMax */
+            scale = scaleMax
+            backgroundToPutOnTop.style.transform = 'scale(' + scale + ')';
+            /* We fadeIn the backgroundToPutOnTop so by the end of currentSection its opacity is one
+            * One the contrary we fade the background currentlyOnTop
+            */
+            opacity = currentSectionWithId.distance / currentSection.clientHeight;
+            backgroundToPutOnTop.style.opacity = opacity;
+            if (backgroundCurrentlyOnTop != undefined) {backgroundCurrentlyOnTop.style.opacity = 1 - opacity} ;
+        } else {
+            if (backgroundCurrentlyOnTop != undefined) {backgroundCurrentlyOnTop.classList.remove("top")} ;
+            backgroundToPutOnTop.classList.add("top");
+        }
+    }
+
+
+    /*
     if(backgroundToPutOnTop != undefined ) {
         if (backgroundsOnTop.length > 0) {
             if (backgroundsOnTop.length > 0 && backgroundsOnTop[0] != backgroundToPutOnTop) {
@@ -52,6 +103,7 @@ function updateBackground(currentSectionWithId) {
 
         zoomInBackground(currentSectionWithId, backgroundToPutOnTop);
     }
+    */
 
 }
 
