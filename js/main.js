fc0814fddd081fe9a758f3d64608a473b3a00bcf
Adrien El Zein <adrizein@gmail.com>
2020-02-17 03:15:28 +0100

    add header and footer and improve fading and zooming

--- js/main.js
+++ js/main.js
@@ -3,26 +3,23 @@ function goTo(url) {
 }
 
 function getCurrentSectionWithId() {
-    var sections = document.getElementsByTagName("section");
-    var currentSectionWithId = {sectionIndex:-1,id:"",distance:0};
-
+    var currentSectionWithId = { sectionIndex: -1, id: "", distance: 0 };
     /*
     * Not all sections have id
     * We want to switch from one id to the other
     * as soon as the top of the section is hit
     */
 
-    for (var i = 0; i < sections.length; i++) {
-        var distance =  -(sections[i].getBoundingClientRect().y);
-        if (sections[i].id && distance >= 0) {
-            if(currentSectionWithId.distance == 0
-                || distance <= currentSectionWithId.distance) {
+    sections.forEach((section, i) => {
+        var distance = -(section.getBoundingClientRect().y);
+        if (section.id && distance >= 0) {
+            if (currentSectionWithId.distance == 0 || distance <= currentSectionWithId.distance) {
                 currentSectionWithId.sectionIndex = i;
                 currentSectionWithId.id = sections[i].id;
                 currentSectionWithId.distance = distance;
             }
         }
-    }
+    });
     return currentSectionWithId;
 }
 
@@ -39,18 +36,20 @@ function updateState(currentSectionWithId) {
     }
 }
 
+
 function updateBackground(currentSectionWithId) {
     /* VINCKY */
 
     /* Where we manage the backgrounds */
-    //console.log(currentSectionWithId);
-    var currentBackground = document.getElementById("bg-"+currentSectionWithId.id);
+    var currentBackground = document.querySelector(`img.background.${currentSectionWithId.id}`);
+    var otherBackgrounds = document.querySelectorAll(`img.background:not(.${currentSectionWithId.id})`);
+
     var currentSection = document.getElementById(currentSectionWithId.id);
 
-    var paddingTop  = window.innerHeight;
     var paddingBottom = window.innerHeight; // can be changed
-    var fadeInAndOutDistance = currentSection.clientHeight - paddingBottom;
-    var zoomInAndOutDistance = paddingBottom/4;
+    var scrollTextLength = currentSection.clientHeight - paddingBottom;
+    var fadeInLength = scrollTextLength / 2;
+    var zoomInAndOutDistance = paddingBottom / 4;
 
 
     /* Here is the sequence 
@@ -61,64 +60,69 @@ function updateBackground(currentSectionWithId) {
     * The zoom in zoom out occurs in the padding bottom
     */
 
-   var scale = 1;
-   var opacity = 1;
-   var scaleMax = 6;
-   var a, b;
+    var scale = 1;
+    var opacity = 1;
+    var scaleMax = 6;
+    var a, b;
     if (currentSectionWithId.sectionIndex == 0) {
         /* scale of 1 at distance 0
-        * scaleMax at home section height + spacer height
+        * scaleMax at home section height
         */
         b = 1
-        a = (scaleMax-1)/(currentSection.clientHeight)
+        a = (scaleMax - 1) / (currentSection.clientHeight)
         scale = a * currentSectionWithId.distance + b
         currentBackground.style.transform = 'scale(' + scale + ')';
         currentBackground.style.opacity = 1;
+        otherBackgrounds.forEach((background) => background.style.opacity = 0);
     } else {
-        var previousBackground = document.getElementById("bg-"+getSectionFromSectionIndex(currentSectionWithId.sectionIndex - 1).id);
-        if (currentSectionWithId.distance < fadeInAndOutDistance) {
+        var previousBackground = currentBackground.previousElementSibling;
+        if (currentSectionWithId.distance < scrollTextLength) {
             /* We don't scale in the section and the scale stays at scaleMax */
             scale = scaleMax
-            currentBackground.style.transform = 'scale(' + scale + ')';
-            previousBackground.style.transform = 'scale(' + scale + ')';
-            /* We fadeIn the backgroundToPutOnTop so by the end of currentSection its opacity is one
-            * One the contrary we fade the background currentlyOnTop
-            */
-            opacity = currentSectionWithId.distance / fadeInAndOutDistance;
-            currentBackground.style.opacity = opacity;
-            previousBackground.style.opacity = 1 - opacity ;
+            backgrounds.forEach((background) => background.style.transform = `scale(${scale})`);
+            otherBackgrounds.forEach((background) => {
+                if (background !== previousBackground) {
+                    background.style.opacity = 0
+                }
+            });
+
+            if (currentSectionWithId.distance > fadeInLength) {
+                /* We fadeIn the backgroundToPutOnTop so by the end of currentSection its opacity is one
+                * One the contrary we fade out the background currentlyOnTop
+                */
+                opacity = (currentSectionWithId.distance - fadeInLength) / fadeInLength;
+                currentBackground.style.opacity = opacity;
+                previousBackground.style.opacity = 1 - opacity;
+            }
         } else {
             currentBackground.style.opacity = 1;
             previousBackground.style.opacity = 0;
 
             // But we zoom out and in in the spacer !
-            var distanceInSpacer = currentSectionWithId.distance - fadeInAndOutDistance;
-            console.log(distanceInSpacer);
-            console.log(paddingBottom);
-            if ( distanceInSpacer < zoomInAndOutDistance) {
+            var distanceInSpacer = currentSectionWithId.distance - scrollTextLength;
+            if (distanceInSpacer < zoomInAndOutDistance) {
                 // Zoom out 
                 //scale =  1 ;
-                console.log("Should be zooming out");
+                console.log("Zooming out");
                 scale = scaleMax + (1 - scaleMax) * distanceInSpacer / zoomInAndOutDistance;
-            } else if (distanceInSpacer > (paddingBottom - zoomInAndOutDistance)){
+            } else if (distanceInSpacer > (paddingBottom - zoomInAndOutDistance)) {
                 // Zoom in 
-                //scale =  scaleMax ;
+                // scale =  scaleMax ;
                 // At paddingBottom we want  scaleMax
                 // At paddingBottom - zoomInAndOutDistance we want 1
                 // 1 = a x (paddingBottom - zoomInAndOutDistance) + b
                 // scaleMax = a x paddingBottom + b
 
-                console.log("Should be zooming in");
-            
-                a = (scaleMax - 1)/(zoomInAndOutDistance);
+                console.log("Zooming in");
+
+                a = (scaleMax - 1) / (zoomInAndOutDistance);
                 b = scaleMax - a * paddingBottom;
-                scale =  b + a * distanceInSpacer;
+                scale = b + a * distanceInSpacer;
             } else {
                 // We are in the "palier" where the scale is one
-                scale =  1;
+                scale = 1;
             }
-            console.log(scale);
-            currentBackground.style.transform = 'scale(' + scale + ')';
+            currentBackground.style.transform = `scale(${scale})`;
         }
     }
 }
@@ -129,14 +133,34 @@ function updateOnScroll() {
     updateState(currentSectionWithId);
 }
 
+function updateLanguage(lang) {
+    languages.forEach((language) => {
+        if (lang === language.id) {
+            language.classList.add('selected');
+        } else {
+            language.classList.remove('selected');
+        }
+    });
+    document.firstElementChild.setAttribute('lang', lang);
+}
 
-function init(){
+var backgrounds, sections, languages;
+function init() {
+    var defaultLanguage = document.firstElementChild.getAttribute('lang');
+    languages = document.querySelectorAll('header .language span');
+    languages.forEach((language) => {
+        language.addEventListener('click', (event) => updateLanguage(event.target.id))
+    });
+    updateLanguage(defaultLanguage);
+
+    backgrounds = document.querySelectorAll(`img.background`);
+    sections = document.querySelectorAll(`#content section`);
     var currentSectionWithId = getCurrentSectionWithId();
     updateBackground(currentSectionWithId);
 
-    var content= document.getElementById("content");
-    content.addEventListener('scroll', updateOnScroll, {passive: true});
-    content.addEventListener('resize', updateOnScroll, {passive: true});
+    var content = document.getElementById("content");
+    content.addEventListener('scroll', updateOnScroll, { passive: true });
+    content.addEventListener('resize', updateOnScroll, { passive: true });
 
     for (const node of document.getElementsByClassName('accordion-item')) {
         node.addEventListener('click', (event) => {
